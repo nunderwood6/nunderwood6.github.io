dbe13873a298427d5ead92bdf7c99c15792a2b50
Nick Underwood <nunderwood6@gmail.com>
2018-12-02 20:03:58 -0600

    Hover only on desktop

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 //get window width
 var w = window.innerWidth;
 
-d3.selectAll("a.portfolio-item")
-		.on("mouseover", function(){
-			d3.select(this).select(".popup").style("opacity", "1");
-
-		})
-		.on("mouseout", function(){
-			d3.select(this).select(".popup").style("opacity", "0")
-		});
 
 if(w<575){
 //if single column use scrolltrigger
@@ -20,17 +12,26 @@ document.addEventListener('DOMContentLoaded', function(){
     },
     offset: {
       x: 0,
-      y: 50
+      y: 0
     },
     addHeight: true,
   }, document.body, window);
 });
+}else{
+
+
+
+d3.selectAll("a.portfolio-item")
+		.on("mouseover", function(){
+			d3.select(this).select(".popup").style("opacity", "1");
+
+		})
+		.on("mouseout", function(){
+			d3.select(this).select(".popup").style("opacity", "0")
+		});
+
+
 }
-
-
-
-
-
 
 
 
