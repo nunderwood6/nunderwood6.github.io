63f6591ece462a647412d64ef1921f3a6aef7204
Nick Underwood <nunderwood6@gmail.com>
2018-12-02 19:39:10 -0600

    Added scrolltriggers for single col layout

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
-
-
-var projects= {
-	"wild": "Wilder than the National Parks",
-	"bitterroot": "Climbing Areas of the Bitteroot",
-	"flyover": "Flyover Country Visualization Suite",
-	"ageless": "The Ageless Alps: A Visual Exploration of Longevity in the Mountains"
-
-};
+//get window width
+var w = window.innerWidth;
 
 d3.selectAll("a.portfolio-item")
 		.on("mouseover", function(){
-			console.log("trigger");
 			d3.select(this).select(".popup").style("opacity", "1");
 
 		})
 		.on("mouseout", function(){
-			console.log("off");
 			d3.select(this).select(".popup").style("opacity", "0")
 		});
+
+if(w<575){
+//if single column use scrolltrigger
+document.addEventListener('DOMContentLoaded', function(){
+  var trigger = new ScrollTrigger({
+    toggle: {
+      visible: 'visibleClass',
+      hidden: 'hiddenClass'
+    },
+    offset: {
+      x: 0,
+      y: 50
+    },
+    addHeight: true,
+  }, document.body, window);
+});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
